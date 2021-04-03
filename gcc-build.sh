5dcf6abb1a284437752fa4228ef2c2cb0f95dd85
Adam Baxter <voltagex@voltagex.org>
2021-04-03 21:12:46 +1100

    Bump versions

--- gcc-build.sh
+++ gcc-build.sh
@@ -9,15 +9,22 @@ export PATH=$LFS/tools/bin:/bin:/usr/bin
 #export GNU=https://ftpmirror.gnu.org/gnu
 export GNU=http://gnu.mirror.constant.com
 
+export BINUTILS_VERSION=2.36.1
+export GCC_VERSION=10.2.1
+export MPFR_VERSION=4.1.0
+export GMP_VERSION=6.2.1
+export MPC_VERSION=1.2.1
+
+
 apt update; apt -y install --no-install-recommends xz-utils gcc g++ bison make curl ca-certificates
 
 mkdir /lfs
 mkdir $LFS/sources
 pushd $LFS/sources
 
-curl -L $GNU/binutils/binutils-2.35.tar.xz | tar -Jxf -
-mkdir binutils-2.35/build
-pushd binutils-2.35/build
+curl -L $GNU/binutils/binutils-$BINUTILS_VERSION.tar.xz | tar -Jxf -
+mkdir binutils-$BINUTILS_VERSION/build
+pushd binutils-$BINUTILS_VERSION/build
 
 ../configure --prefix=$LFS/tools       \
              --with-sysroot=$LFS        \
@@ -28,16 +35,16 @@ make -j24 &> make-binutils-output.log
 make install > /dev/null
 popd
 
-curl -sL $GNU/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz | tar -Jxf -
-pushd gcc-10.2.0
+curl -sL $GNU/gcc/gcc-${GCC_VERSION}/gcc-$GCC_VERSION.tar.xz | tar -Jxf -
+pushd gcc-$GCC_VERSION
 
-curl -sL $GNU/mpfr/mpfr-4.1.0.tar.xz | tar -Jxf -
-curl -sL $GNU/gmp/gmp-6.2.0.tar.xz | tar -Jxf -
-curl -sL $GNU/mpc/mpc-1.2.0.tar.gz | tar -zxf -
+curl -sL $GNU/mpfr/mpfr-$MPFR_VERSION.tar.xz | tar -Jxf -
+curl -sL $GNU/gmp/gmp-$GMP_VERSION.tar.xz | tar -Jxf -
+curl -sL $GNU/mpc/mpc-$MPC_VERSION.tar.gz | tar -zxf -
 
-mv -v mpfr-4.1.0 mpfr
-mv -v gmp-6.2.0 gmp
-mv -v mpc-1.2.0 mpc
+mv -v mpfr-$MPFR_VERSION mpfr
+mv -v gmp-$GMP_VERSION gmp
+mv -v mpc-$MPC_VERSION mpc
 
 mkdir build
 pushd build
@@ -63,6 +70,6 @@ pushd build
     --disable-libstdcxx                            \
     --enable-languages=c,c++ &> configure-gcc-output.log
 
-make -j24 &> gcc-build-output.log
+make -j &> gcc-build-output.log
 
 
