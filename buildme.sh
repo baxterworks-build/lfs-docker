dc2b3f79590a9657314dd305ba7d7533c399adb1
Adam Baxter <git@voltagex.org>
2022-02-24 16:38:19 +1100

    Fix Docker command

--- buildme.sh
+++ buildme.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash -x
-docker build --build-arg GNU=https://mirror.aarnet.edu.au/pub/gnu/ -t voltagex/lfs-gcc-1 . --volume $PWD/output:/lfs 
+docker build --build-arg GNU=https://mirror.aarnet.edu.au/pub/gnu/ -t voltagex/lfs-gcc-1 .
